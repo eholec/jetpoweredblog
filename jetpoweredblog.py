3542fb59a9f6b0a42a08dd7eb9431e8a004d253d
Eric.Holec <ericholec@gmail.com>
2013-10-28 21:55:48 -0500

    Hello world!

---
+++ jetpoweredblog.py
@@ -0,0 +1,13 @@
+import webapp2
+
+
+class HomePage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+application = webapp2.WSGIApplication([
+    ('/', HomePage),
+], debug=True)
