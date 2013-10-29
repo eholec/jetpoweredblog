bfd466694343a51b4598ee983af8f45a0b45d36b
Eric.Holec <ericholec@gmail.com>
2013-10-28 22:32:21 -0500

    Added Post class.

---
+++ post.py
@@ -0,0 +1,8 @@
+import webapp2
+from google.appengine.ext import ndb
+
+
+class Post(ndb.Model):
+    title = ndb.StringProperty()
+    content = ndb.TextProperty()
+    postedDate = ndb.DateTimeProperty(auto_now_add=True)
