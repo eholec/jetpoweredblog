1198c6d313b4cb2a83f80bc6050baf5f0716ef59
Eric.Holec <ericholec@gmail.com>
2013-10-29 21:44:16 -0500

    Home page now lists posts.

--- jetpoweredblog.py
+++ jetpoweredblog.py
@@ -1,11 +1,17 @@
 import webapp2
+from post import Post
 
 
 class HomePage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        posts = Post.query().order(-Post.postedDate)
+        for post in posts:
+            self.response.write(
+                post.title + ' ' +
+                post.postedDate.strftime('%c') + ': ' +
+                post.content + '\n')
 
 
 application = webapp2.WSGIApplication([
