7ed10647c09d2eeb2dbf4b9572c60738d571d349
eholec <ericholec@gmail.com>
2013-10-31 20:25:11 -0500

    Home page now responds with html.

--- jetpoweredblog.py
+++ jetpoweredblog.py
@@ -5,13 +5,16 @@ from post import Post
 class HomePage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.headers['Content-Type'] = 'text/html'
         posts = Post.query().order(-Post.postedDate)
+
+        self.response.write('<!DOCTYPE html><html><body>')
         for post in posts:
-            self.response.write(
-                post.title + ' ' +
-                post.postedDate.strftime('%c') + ': ' +
-                post.content + '\n')
+            self.response.write('<h2>' + post.title + '</h2>')
+            self.response.write('<h3>' +
+                    post.postedDate.strftime('%c') + '</h3>')
+            self.response.write('<p>' + post.content + '</p>')
+        self.response.write('</body></html>')
 
 
 application = webapp2.WSGIApplication([
