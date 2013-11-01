e9de3cf75c2e75af584652c81aa8e4fdc70c68bb
eholec <ericholec@gmail.com>
2013-10-31 21:04:01 -0500

    Added admin page to create posts.

--- jetpoweredblog.py
+++ jetpoweredblog.py
@@ -1,4 +1,5 @@
 import webapp2
+from google.appengine.api import users
 from post import Post
 
 
@@ -17,6 +18,41 @@ class HomePage(webapp2.RequestHandler):
         self.response.write('</body></html>')
 
 
+class AdminPage(webapp2.RequestHandler):
+
+    def get(self):
+        user = users.get_current_user()
+        if not user or not users.is_current_user_admin():
+            self.redirect(users.create_login_url(self.request.uri))
+            return
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<!DOCTYPE html><html><body>')
+        self.response.write('<form action="/admin" method="post">')
+        self.response.write('<input type="text" name="title"><br>')
+        self.response.write('<textarea name="content" rows="10" cols="60">' +
+                '</textarea><br>')
+        self.response.write('<input type="submit" value="Create Post">')
+        self.response.write('</form>')
+        self.response.write('</body></html>')
+
+    def post(self):
+        user = users.get_current_user()
+        if not user or not users.is_current_user_admin():
+            self.redirect(users.create_login_url(self.request.uri))
+            return
+
+        post = Post(title=self.request.get('title'),
+            content=self.request.get('content'))
+        post.put()
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<!DOCTYPE html><html><body>')
+        self.response.write('<p>Post created</p>')
+        self.response.write('</body></html>')
+
+
 application = webapp2.WSGIApplication([
     ('/', HomePage),
+    ('/admin', AdminPage),
 ], debug=True)
