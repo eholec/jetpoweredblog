1af490ef8a0a0ac9029967c7247c115e1358f266
eholec <ericholec@gmail.com>
2013-11-14 20:11:07 -0600

    Added single post pages.

--- jetpoweredblog.py
+++ jetpoweredblog.py
@@ -2,6 +2,7 @@ import os
 
 import webapp2
 import jinja2
+from google.appengine.api import users
 
 import admin.posts
 from post import Post
@@ -25,8 +26,32 @@ class HomePage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
+class PostPage(webapp2.RequestHandler):
+    def get(self, id):
+        post = Post.get_by_id(int(id))
+
+        if post is None:
+            self.response.set_status(404)
+            return
+
+        if not post.isPosted:
+            user = users.get_current_user()
+            if not user or not users.is_current_user_admin():
+                self.response.set_status(404)
+                return
+
+        template = JINJA_ENVIRONMENT.get_template(
+            'templates/post.html')
+        template_values = {
+            'post': post,
+        }
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(template_values))
+
 application = webapp2.WSGIApplication([
     (r'/', HomePage),
+    (r'/posts/(\d+)', PostPage),
     (r'/admin/posts', admin.posts.AdminPostsHandler),
     (r'/admin/posts/create', admin.posts.AdminPostsCreateHandler),
     (r'/admin/posts/edit/(\d+)', admin.posts.AdminPostsEditHandler),
